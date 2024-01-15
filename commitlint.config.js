12b44dafb613a373952a3e8a1b70a9ed8debf271
Teodor92 <teodor.ivanov92@gmail.com>
2024-01-15 13:22:52 +0200

    chore: added latest version of husky and commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
-};
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'feat',
+          'fix',
+          'docs',
+          'chore',
+          'style',
+          'refactor',
+          'ci',
+          'test',
+          'revert',
+          'perf',
+          'vercel',
+        ],
+      ],
+    },
+  };
